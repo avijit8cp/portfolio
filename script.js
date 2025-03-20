e50603c919a27a6eb08a079d750a0aa8c35b8877
avijit8cp <avijitavijit806@gmail.com>
2025-03-20 21:50:20 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//github
+    var github = document.getElementById("github");
+    github.onclick = function(){
+        window.location.href="https://github.com/avijit8cp";
+    }
+
+//facebook
+
+var facebook = document.getElementById("facebook");
+facebook.onclick = function(){
+    window.location.href="https://www.facebook.com/share/1By3oHFaLq/";
+}
+
+//instagram
+var insta = document.getElementById("insta");
+insta.onclick = function(){
+    window.location.href="https://www.instagram.com/avijitchakraborty683/profilecard/?igsh=ZDVubWszMnAwYnZn";
+}
+
+//footer project
+
+var footer_pro = document.getElementById("footer-pro");
+footer_pro.onclick = function(){
+    window.location.href="https://github.com/avijit8cp?tab=repositories";
+}
+
+
+
+
