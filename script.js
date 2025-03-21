d4200c30b2231e3d01f575f6582013af2d99b7b9
avijit8cp <avijitavijit806@gmail.com>
2025-03-21 13:47:45 +0530

    footer icon fix

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
 //github
-    var github = document.getElementById("github");
-    github.onclick = function(){
+    var github = document.getElementById("github").addEventListener("click", function(){
         window.location.href="https://github.com/avijit8cp";
-    }
+    });
 
-    var foot_github = document.getElementById("foot-github");
-    foot_github.onclick = function(){
-        window.location.href="https://github.com/avijit8cp";
-    }
+
+   
 
 //facebook
 
-var facebook = document.getElementById("facebook");
-facebook.onclick = function(){
+var facebook = document.getElementById("facebook").addEventListener("click", function(){
     window.location.href="https://www.facebook.com/share/1By3oHFaLq/";
-}
+});
 
-var foot_facebook = document.getElementById("foot-facebook");
-foo_tfacebook.onclick = function(){
-    window.location.href="https://www.facebook.com/share/1By3oHFaLq/";
-}
 
 //instagram
-var insta = document.getElementById("insta");
-insta.onclick = function(){
-    window.location.href="https://www.instagram.com/avijitchakraborty683/profilecard/?igsh=ZDVubWszMnAwYnZn";
-}
 
-var foot_insta = document.getElementById("foot-insta");
-foot_insta.onclick = function(){
+var insta = document.getElementById("insta").addEventListener("click", function(){
     window.location.href="https://www.instagram.com/avijitchakraborty683/profilecard/?igsh=ZDVubWszMnAwYnZn";
-}
+});
+
+
 
 //footer project
 
-var footer_pro = document.getElementById("footer-pro");
-footer_pro.onclick = function(){
+var footer_pro = document.getElementById("footer-pro").addEventListener("click", function(){
     window.location.href="https://github.com/avijit8cp?tab=repositories";
-}
+});
+
+
+
+
 
 
 
