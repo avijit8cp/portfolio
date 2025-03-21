f7dd193526ee3facf2f2cefef91419674cf0ad4f
avijit8cp <avijitavijit806@gmail.com>
2025-03-21 14:01:40 +0530

    2nd commit

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location.href="https://github.com/avijit8cp";
     });
 
+    var foot_github = document.getElementById("foot_github").addEventListener("click", function(){
+        window.location.href="https://github.com/avijit8cp";
+    });
+
 //facebook
 
 var facebook = document.getElementById("facebook").addEventListener("click", function(){
     window.location.href="https://www.facebook.com/share/1By3oHFaLq/";
 });
+
+var foot_facebook = document.getElementById("foot_facebook").addEventListener("click", function(){
+    window.location.href="https://www.facebook.com/share/1By3oHFaLq/";
+});
+
 
 
 //instagram
@@ -17,6 +26,10 @@ var insta = document.getElementById("insta").addEventListener("click", function(
     window.location.href="https://www.instagram.com/avijitchakraborty683/profilecard/?igsh=ZDVubWszMnAwYnZn";
 });
 
+
+var foot_insta = document.getElementById("foot_insta").addEventListener("click", function(){
+    window.location.href="https://www.instagram.com/avijitchakraborty683/profilecard/?igsh=ZDVubWszMnAwYnZn";
+});
 
 
 //footer project
